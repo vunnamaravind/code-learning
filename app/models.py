db2e1d3e737b41cfe04755947aa59301a25c1ec7
aravind <aravindvunnam95@gmail.com>
2020-12-24 19:48:07 +0530

    added pagination  and created user_posts.html

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from flask_login import UserMixin
 from app import db, login_manager
+from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return  User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
